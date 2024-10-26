beaffff925fa8928967cde12bac741bf22c23351
alinidi <alinareyz@gmail.com>
2024-10-26 18:29:17 +0500

    (init: started/ended the task (oct 26, 2024, 6:30:05PM)

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function sliceTheExpr(expr){
+        const result = [];
+        for (let i = 0; i < expr.length; i+=10){
+            let block = expr.slice(i, i+10);
+            result.push(block);
+        }
+        return result;
+    }
+    
+    function convert (block){
+        if(block === '**********'){
+            return ' '
+        }
+        let morseCode = '';
+        for (let i = 0; i<block.length; i+=2){
+            const couple = block.slice(i, i+2);
+            if(couple === '10'){
+                morseCode += '.';
+            } else if (couple === '11'){
+                morseCode += '-';
+            }
+        }
+        return morseCode;
+    }
+
+    const blocks = sliceTheExpr(expr);
+    const decoded = blocks.map(block => {
+        const morseCode = convert (block);
+        return morseCode === ' ' ? ' ':MORSE_TABLE[morseCode];
+    }).join('');
+
+    return decoded
 }
 
 module.exports = {
